0f8d6f2bb2e8306ef8b26e66d026f997605c14dd
1097195326 <1097195326@qq.com>
2021-01-26 20:56:19 +0800

    ss

--- Source/SmallWorld/Private/GameActors/Soldiers/Horseman.cpp
+++ Source/SmallWorld/Private/GameActors/Soldiers/Horseman.cpp
@@ -12,14 +12,25 @@ AHorseman::AHorseman()
 bool AHorseman::TryMoveSoldier()
 {
 	int32 MoveDis = GetSoldierData()->GetMoveRange();
+	int32 VisibilityDis = GetSoldierData()->GetVisibleRange();
 	AGroundTileActor* MainTile = GetOriginGroundTile();
 	TArray<AGroundTileActor*>  AroundTiles;
 	GameManager::GetGroundTileAroundSoldier(MainTile, MoveDis, AroundTiles);
-	if (AroundTiles.Num() > 0)
+	TArray<TileMapStruct> ArrayTileMap = GameManager::GetGroundTilesNearSoldiers(AroundTiles, VisibilityDis, this, true);
+	AGroundTileActor * FindTile = GameManager::GetGroundTileWithSoldiersNum(ArrayTileMap, false);
+
+	AGroundTileActor * ToggleTile = nullptr;
+	if (FindTile)
 	{
-		int32 TileIndex = UKismetMathLibrary::RandomInteger(AroundTiles.Num());
-		AGroundTileActor * TileActor = AroundTiles[TileIndex];
-		TargetGroundTile = TileActor;
+		ToggleTile = FindTile;
+	}
+	else
+	{
+		ToggleTile = GameManager::GetGroundTileWithDistance(MainTile, AroundTiles);
+	}
+	if (ToggleTile)
+	{
+		TargetGroundTile = ToggleTile;
 		OriginGroundTile->SetBusy(true);
 		TargetGroundTile->SetBusy(true);
 		return true;
--- Source/SmallWorld/Private/GameActors/Soldiers/Knight.cpp
+++ Source/SmallWorld/Private/GameActors/Soldiers/Knight.cpp
@@ -26,14 +26,25 @@ void AKnight::On_Start()
 bool AKnight::TryMoveSoldier()
 {
 	int32 MoveDis = GetSoldierData()->GetMoveRange();
+	int32 VisibilityDis = GetSoldierData()->GetVisibleRange();
 	AGroundTileActor* MainTile = GetOriginGroundTile();
 	TArray<AGroundTileActor*>  AroundTiles;
 	GameManager::GetGroundTileAroundSoldier(MainTile, MoveDis, AroundTiles);
-	if (AroundTiles.Num() > 0)
+	TArray<TileMapStruct> ArrayTileMap = GameManager::GetGroundTilesNearSoldiers(AroundTiles, VisibilityDis, this, true);
+	AGroundTileActor * FindTile = GameManager::GetGroundTileWithSoldiersNum(ArrayTileMap, false);
+
+	AGroundTileActor * ToggleTile = nullptr;
+	if (FindTile)
 	{
-		int32 TileIndex = UKismetMathLibrary::RandomInteger(AroundTiles.Num());
-		AGroundTileActor * TileActor = AroundTiles[TileIndex];
-		TargetGroundTile = TileActor;
+		ToggleTile = FindTile;
+	}
+	else
+	{
+		ToggleTile = GameManager::GetGroundTileWithDistance(MainTile, AroundTiles);
+	}
+	if (ToggleTile)
+	{
+		TargetGroundTile = ToggleTile;
 		OriginGroundTile->SetBusy(true);
 		TargetGroundTile->SetBusy(true);
 		return true;
